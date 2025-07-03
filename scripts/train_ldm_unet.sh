f2c647f0aecb934e63b560d8c945dde928da0b46
Akul <dogmousecat88@gmail.com>
2025-07-03 07:10:13 -0500

    Warning removal

--- scripts/train_ldm_unet.sh
+++ scripts/train_ldm_unet.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
+
+VAE_CHECKPOINT="../model_runs/vae_run_3/vae_best.pth" 
+
 python ../train_ldm_unet.py \
     --latent_data_dir ../data \
-    --save_dir ../model_runs/ldm_unet_run_2 \
+    --save_dir ../model_runs/ldm_unet_run_6 \
+    --vae_checkpoint $VAE_CHECKPOINT \
     --latent_dim 32 \
     --latent_size 16 \
-    --epochs 100 \
+    --epochs 300 \
     --batch_size 32 \
     --lr 1e-4 \
     --num_workers 4 \
     --noise_steps 1000 \
-    --noise_schedule cosine
+    --noise_schedule cosine \
+    --save_interval 10 
